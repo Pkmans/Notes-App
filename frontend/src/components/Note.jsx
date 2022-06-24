4b5658b86d2084b2000f0b3a77559fd6c87a6a82
Aaron Lam <aaronlam2k@gmail.com>
2022-06-23 23:26:22 -0700

    Add Draggable notes feature, UI updates

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Draggable from 'react-draggable';
 import EditPopup from './EditPopup';
 import DeletePopup from './DeletePopup';
+import LockIcon from '@mui/icons-material/Lock';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
 
-function Note (props) {
+function Note(props) {
+    const [dragDisabled, setDragDisabled] = useState(true);
+
+    function handleClick() {
+        setDragDisabled(prevValue => {
+            return !prevValue;
+        })
+    }
+
     function editNote(input) {
-        props.editNote(input, props.id, {title: props.title, content: props.content});
+        props.editNote(input, props.id, { title: props.title, content: props.content });
     }
 
     function deleteNote(isConfirmed) {
-        props.deleteNote(props.id, {title: props.title, content: props.content}, isConfirmed);
+        props.deleteNote(props.id, { title: props.title, content: props.content }, isConfirmed);
     }
-    
-    return <div className='note'>
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
-        <DeletePopup deleteNote={deleteNote}/>
 
-        <EditPopup title={props.title} content={props.content} editNote={editNote}/>
-    </div>
+    return <Draggable disabled={dragDisabled}>
+        <div className='note'>
+        
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+
+            <button onClick={handleClick}>
+                {dragDisabled ? <LockIcon /> : <LockOpenIcon />}
+            </button>
+            <EditPopup title={props.title} content={props.content} editNote={editNote} />
+            <DeletePopup deleteNote={deleteNote} />
+            
+        </div>
+    </Draggable>
 }
 
 export default Note;
