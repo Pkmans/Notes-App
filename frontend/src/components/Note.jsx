407b152b161f8f2c8fe7f61158ae8c5f1fe03d1b
Aaron Lam <aaronlam2k@gmail.com>
2022-06-24 16:50:12 -0700

    Added handlebar for draggable notes, CSS and UI updates

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -4,6 +4,7 @@ import EditPopup from './EditPopup';
 import DeletePopup from './DeletePopup';
 import LockIcon from '@mui/icons-material/Lock';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import axios from 'axios';
 
 function Note(props) {
@@ -20,18 +21,18 @@ function Note(props) {
 
     function firstDrag(event) {
         if (!beenDragged) {
-            axios.post("api/note/beenDragged", {id: props.id})
+            axios.post("api/note/beenDragged", { id: props.id })
                 .then(setBeenDragged(true));
         }
     }
 
     function finishDrag(event, data) {
         setPosition({ xPos: data.x, yPos: data.y });
-        
+
     }
 
     useEffect(() => {
-        axios.post("/api/note/updateposition", {position, id: props.id });
+        axios.post("/api/note/updateposition", { position, id: props.id });
     }, [position]);
 
     // Helper Functions
@@ -40,30 +41,29 @@ function Note(props) {
     }
 
     function deleteNote(isConfirmed) {
-        console.log(props.id);
         props.deleteNote(props.id, isConfirmed);
     }
 
     return <Draggable
         disabled={dragDisabled}
+        handle="strong"
         onStart={firstDrag}
         onStop={finishDrag}
         defaultPosition={{ x: props.xPos, y: props.yPos }}
-        // position={location}
     >
-        <div 
-        className='note' 
-        // style={{ position: beenDragged ? 'absolute' : 'static'}}
-        >
+        <div className='note'>
+            <strong style={{cursor: !dragDisabled && "move"}}><ArrowDropUpIcon className="arrow" /></strong>
 
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
+            <div className="noteBox">
+                <h1>{props.title}</h1>
+                <p>{props.content}</p>
 
-            <button onClick={handleClick}>
-                {dragDisabled ? <LockIcon /> : <LockOpenIcon />}
-            </button>
-            <EditPopup title={props.title} content={props.content} editNote={editNote} />
-            <DeletePopup deleteNote={deleteNote} />
+                <button onClick={handleClick}>
+                    {dragDisabled ? <LockIcon /> : <LockOpenIcon />}
+                </button>
+                <EditPopup title={props.title} content={props.content} editNote={editNote} />
+                <DeletePopup deleteNote={deleteNote} />
+            </div>
 
         </div>
     </Draggable>
